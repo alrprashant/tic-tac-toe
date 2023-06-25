585d5df9d149ff9f3003f826b0d631c045eb3976
alrprashant <alrprashant009@gmail.com>
2023-06-25 11:58:32 +0530

    commited

--- script.js
+++ script.js
@@ -16,27 +16,82 @@ const winningPositions=[
 function initGame(){
     currentPlayer="X";
     gameGrid=["","","","","","","","",""];
+    boxes.forEach((box,index)=>{
+        box.innerText="";
+        boxes[index].style.pointerEvents="all";
+        box.classList=`box box${index+1}`;
+    })
     newGameBtn.classList.remove("active");
     gameInfo.innerText=`Current Player -${currentPlayer}`;
 
 }
 initGame();
+function checkGameOver(){
+    let answer = "";
+    winningPositions.forEach((position)=>{
+        if((gameGrid[position[0]] !==""||gameGrid[position[1]] !==""||gameGrid[position[2]] !=="")
+        &&(gameGrid[position[0]]===gameGrid[position[1]])&&(gameGrid[position[1]]===gameGrid[position[2]])
+        ){
+            if(gameGrid[position[0]]==="X"){
+                answer="X"
+            }
+            else{
+                answer="O";
+            }
+            //disable pointer events
+            boxes.forEach((box) => {
+                box.style.pointerEvents = "none";
+            })
+            //  winner color change 
+            boxes[position[0]].classList.add("win");
+            boxes[position[1]].classList.add("win");
+            boxes[position[2]].classList.add("win");
+
+
+        }
+    })
+    if(answer!==""){
+        gameInfo.innerText=`Winner player-${answer}`;
+        newGameBtn.classList.add("active");
+
+    }
+    // here we go to find is game tie?
+    let fillCount = 0;
+    gameGrid.forEach((box) => {
+        if(box !== "" )
+            fillCount++;
+    })
+    
+    if(fillCount === 9) {
+        gameInfo.innerText = "Game Tied !";
+        newGameBtn.classList.add("active");
+    }
+
+}
 function swapTurn(){
     if(currentPlayer === "X"){
         currentPlayer ="O" ;
     }else{
         currentPlayer="X";
     }
+    // UI update
     gameInfo.innerText=`Current Player -${currentPlayer}`;
+
     }
 
 
 function handleClick(index){
     if(gameGrid[index] ===""){
         boxes[index].innerText=currentPlayer;
+        boxes[index].style.pointerEvents="none";
         gameGrid[index]=currentPlayer;
         //  swap karo 
         swapTurn();
+        // to check game is over or not
+        checkGameOver();
+
+
+
 
         
 
@@ -49,5 +104,7 @@ boxes.forEach((box,index)=>{
     })
 
 });
+
+newGameBtn.addEventListener("click",initGame);
 
 
